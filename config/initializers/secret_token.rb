f97ebde2c4b51d3c63ef879f22bce058b09ebc0c
Hubert Lee <hubert.k.lee@gmail.com>
2014-02-28 20:46:14 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyCal::Application.config.secret_key_base = '27f1086a3242ccd18c10b9b3f412ec5ccd9e97a4bcccfca9a382d886164fe5ffec9d22b3fa5e2435f49dcb9c67cbc0686dad7708710d6aedd569c476318a16bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyCal::Application.config.session_store :cookie_store, key: '_easy_cal_session'
