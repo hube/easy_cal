28570fca21a2ad347b1e92638147eac17021cfe1
Hubert Lee <hubert.k.lee@gmail.com>
2014-03-02 21:04:45 +0800

    use config to set i18n

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module EasyCal
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    I18n.enforce_available_locales = true
+    config.i18n.enforce_available_locales = true
   end
 end
