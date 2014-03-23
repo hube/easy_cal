081b09f7fa09adc8b98d9280eddfb37d66eca46f
Hubert Lee <hubert.k.lee@gmail.com>
2014-03-24 00:54:16 +0800

    add view to show event details

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,21 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
     @event = Event.new
+  end
+
+  def create
+    redirect_to Event.create!(event_params)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :duration)
   end
 end
