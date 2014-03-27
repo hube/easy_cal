e0ab6734064b47028dd81e063cba3337713c2d0d
Hubert Lee <hubert.k.lee@gmail.com>
2014-03-27 10:35:59 +0800

    remove duration from controllers and view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:date, :duration)
+    params.require(:event).permit(:date)
   end
 end
